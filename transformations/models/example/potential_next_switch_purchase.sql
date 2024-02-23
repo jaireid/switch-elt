964cfe10a0ab823ed270662e90364545b233809e
jaireid <jaiwilliamsreid@gmail.com>
2024-02-23 02:23:40 -0600

    Transformation query to identify next switch purchase

---
+++ transformations/models/example/potential_next_switch_purchase.sql
@@ -0,0 +1,20 @@
+SELECT
+  s.switch_id,
+  s.switch_name,
+  s.manufacturer,
+  s.spring_force,
+  s.stem_type,
+  s.stem_material,
+  s.top_housing,
+  s.bottom_housing,
+  s.rating,
+  s.own_status
+FROM {{ ref('switches') }} s
+JOIN {{ ref('best_rated_switch_characteristics') }} b
+  ON s.switch_type = b.switch_type
+  AND s.spring_force = b.spring_force
+  AND s.stem_type = b.stem_type
+  AND s.stem_material = b.stem_material
+  AND s.top_housing = b.top_housing
+  AND s.bottom_housing = b.bottom_housing
+WHERE s.own_status = 'Unowned'
