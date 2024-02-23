291f18842361a879bb83b8a80ada579ae5573d62
jaireid <jaiwilliamsreid@gmail.com>
2024-02-23 04:13:22 -0600

    Bug
    Fix name typo
    Update own status value to boolean

--- transformations/models/example/potential_next_switch_purchase.sql
+++ transformations/models/example/potential_next_switch_purchase.sql
@@ -11,10 +11,10 @@ SELECT
   s.own_status
 FROM {{ ref('switches') }} s
 JOIN {{ ref('best_rated_switch_characteristics') }} b
-  ON s.switch_type = b.switch_type
+  ON s.switch_name = b.switch_name
   AND s.spring_force = b.spring_force
   AND s.stem_type = b.stem_type
   AND s.stem_material = b.stem_material
   AND s.top_housing = b.top_housing
   AND s.bottom_housing = b.bottom_housing
-WHERE s.own_status = 'Unowned'
+WHERE s.own_status = FALSE
