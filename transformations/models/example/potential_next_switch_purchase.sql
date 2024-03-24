dc4d49f3cf42ed788747d1d81e2059b7f32ec0e0
jaireid <jaiwilliamsreid@gmail.com>
2024-03-24 01:47:19 -0500

    Update potential_next_switch_purchase.sql

--- transformations/models/example/potential_next_switch_purchase.sql
+++ transformations/models/example/potential_next_switch_purchase.sql
@@ -7,14 +7,11 @@ SELECT
   s.stem_material,
   s.top_housing,
   s.bottom_housing,
-  s.rating,
   s.own_status
 FROM {{ ref('switches') }} s
 JOIN {{ ref('best_rated_switch_characteristics') }} b
-  ON s.switch_name = b.switch_name
-  AND s.spring_force = b.spring_force
+  ON s.spring_force = b.spring_force
   AND s.stem_type = b.stem_type
   AND s.stem_material = b.stem_material
-  AND s.top_housing = b.top_housing
-  AND s.bottom_housing = b.bottom_housing
 WHERE s.own_status = FALSE
+LIMIT 3
