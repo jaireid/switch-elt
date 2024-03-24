83715c33a9cb6dd690d64983b1069d64bfb862bd
jaireid <jaiwilliamsreid@gmail.com>
2024-03-24 01:43:38 -0500

    Update best_rated_switch_characteristics.sql

--- transformations/models/example/best_rated_switch_characteristics.sql
+++ transformations/models/example/best_rated_switch_characteristics.sql
@@ -1,11 +1,7 @@
-SELECT
-  switch_name,
-  spring_force,
-  stem_type,
-  stem_material,
-  top_housing,
-  bottom_housing,
-  avg_rating
-FROM {{ ref('switch_ratings') }}
-ORDER BY avg_rating DESC
-LIMIT 1
+WITH best_rated_switch_characteristics AS (
+  SELECT *
+  FROM {{ ref('switch_ratings') }}
+  ORDER BY avg_rating DESC
+  LIMIT 10
+)
+SELECT * FROM best_rated_switch_characteristics
