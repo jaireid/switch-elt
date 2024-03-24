dd4ea3723cbf9caa8c7f0684657cc77d84a180bd
jaireid <jaiwilliamsreid@gmail.com>
2024-03-24 01:46:40 -0500

    Update switch_ratings.sql

--- transformations/models/example/switch_ratings.sql
+++ transformations/models/example/switch_ratings.sql
@@ -1,16 +1,15 @@
-SELECT
-  switch_name,
-  spring_force,
-  stem_type,
-  stem_material,
-  top_housing,
-  bottom_housing,
-  ROUND(AVG(rating), 2) AS avg_rating
-FROM {{ ref('switches') }}
-GROUP BY
-  switch_name,
-  spring_force,
-  stem_type,
-  stem_material,
-  top_housing,
-  bottom_housing
+WITH switch_ratings AS (
+  SELECT
+    spring_force,
+    stem_type,
+    stem_material,
+    ROUND(AVG(rating), 2) AS avg_rating
+  FROM {{ ref('switches') }}
+  WHERE rating IS NOT NULL  -- Exclude NULL ratings
+  GROUP BY
+    spring_force,
+    stem_type,
+    stem_material
+  ORDER BY avg_rating DESC
+)
+SELECT * FROM switch_ratings
